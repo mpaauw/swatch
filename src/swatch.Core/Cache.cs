0d808facad89e4b67c5958128430b17740d17a0a
mpaauw <matt.paauw@gmail.com>
2019-05-10 12:40:40 -0700

    Pushing latest.

---
+++ src/swatch.Core/Cache.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace swatch.Core
+{
+    public class Cache<TKey, TValue> : ICache<TKey, TValue>
+    {
+        
+
+
+
+        public TValue Get(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue Delete(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/swatch.Core/ICache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace swatch.Core
+{
+    public interface ICache<TKey, TValue>
+    {
+        TValue Get(TKey key);
+
+        void Set(TKey key, TValue value);
+
+        TValue Delete(TKey key);
+    }
+}
